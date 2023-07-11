3fb8425101b792e7ece95c62a34026c41ba70dab
musamM921 <135432572+musamM921@users.noreply.github.com>
2023-07-11 14:24:30 +0400

    update5

--- jsdatatypes1/assets/js/index.js
+++ jsdatatypes1/assets/js/index.js
@@ -8,3 +8,27 @@ let phrase = '"Преступление и Наказание" Федор Дос
 let template = `hello ${string} how are you doing`;
 
 console.log(template);
+
+//bool lie
+let bool = true;
+let lie = false;
+console.log(5 < 10);
+
+let object = {
+    a: 5,
+    b: 'javascript',
+    c: false,
+    d: 10,
+    e: 15
+    
+}
+
+object.x = ['a', 'b', 'c'] 
+
+
+console.log(object);
+
+//массив
+let array = ['a', 'b', 'c'];
+
+console.log(array);
